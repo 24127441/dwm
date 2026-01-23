dc86b960351b30049d7e8c9e22917d73434b822f
TheBrilliantCoder <vdloc2431@clc.fitus.edu.vn>
2026-01-23 11:45:18 +0700

    add bar-height patch

--- config.def.h
+++ config.def.h
@@ -10,12 +10,13 @@ static unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char font[]            = "FiraCode Nerd Font Mono:size=14:antialias=true:autohint=true";
-static char dmenufont[]       = "monospace:size=14";
-static const char *fonts[]          = { 
-  "Symbols Nerd Font:size=14",
-  "JetBrainsMono Nerd Font:size=14:antialias=true:autohint=true",
-  "monospace:size=14",
+static const int user_bh            = 6;        /* 2 is the default spacing around the bar's font */
+static char font[]            = "FiraCode Nerd Font Mono:size=12:antialias=true:autohint=true";
+static char dmenufont[]       = "monospace:size=12";
+static const char *fonts[]          = {
+  "Symbols Nerd Font:size=12",
+  "JetBrainsMono Nerd Font:size=12:antialias=true:autohint=true",
+  "monospace:size=12",
 };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
@@ -37,12 +38,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-  { "st-256color",     NULL,        NULL,       0,            0,           1,           0,         -1 },
-  { "librewolf",    NULL,       NULL,    0,           0,            0,            1,           -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class          instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",         NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Firefox",      NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+  { "st",           NULL,     NULL,           0,         0,          1,           0,        -1 },
+  { "librewolf",    NULL,     NULL,           0,         0,          0,           1,        -1 },
+  { "fcitx",        NULL,     NULL,           0,         1,          0,           1,        -1 },
+	{ NULL,           NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
@@ -143,7 +145,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} },
   { 0,                            XF86XK_AudioRaiseVolume, spawn,          {.v = upvol } },
   { 0,                            XF86XK_AudioLowerVolume, spawn,          {.v = downvol } },
   { 0,                            XF86XK_AudioMute,        spawn,          {.v = mutevol } },
